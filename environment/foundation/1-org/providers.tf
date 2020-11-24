4f555e9cd06ff2f8eb6f28ca2212f4d9ba773696
Strong <astrong@Burwood.com>
2020-11-24 16:08:58 -0600

    added backend config

--- environment/foundation/1-org/providers.tf
+++ environment/foundation/1-org/providers.tf
@@ -3,7 +3,10 @@
 # ------------------------------------------------------------
 
 terraform {
-  backend "gcs" {}
+  backend "gcs" {
+    bucket = "lsst-terraform-state"
+    prefix = "foundation/1-org"
+  }
   required_providers {
     google      = "~> 3.1"
     google-beta = "~> 3.1"
