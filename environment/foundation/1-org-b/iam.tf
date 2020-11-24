38dddcea9db4bf950bcd84ed579c22f103e41b2e
Strong <astrong@Burwood.com>
2020-11-24 16:49:54 -0600

    new tf for folder level iam permissions

---
+++ environment/foundation/1-org-b/iam.tf
@@ -0,0 +1,59 @@
+// Qserv Folder
+resource "google_folder_iam_member" "gcp_qserv_clustername_admins_iam_permissions" {
+  for_each = toset(var.gcp_qserv_clustername_admins_iam_permissions)
+  folder  = data.google_active_folder.qserv_sub_folder.name
+  role    = each.value
+  member  = "group:${module.constants.values.gcp_qserv_clustername_admins}"
+}
+
+resource "google_folder_iam_member" "gcp_qserv_clustername_developer_iam_permissions" {
+  for_each = toset(var.gcp_qserv_clustername_admins_iam_permissions)
+  folder  = data.google_active_folder.qserv_sub_folder.name
+  role    = each.value
+  member  = "group:${module.constants.values.gcp_qserv_clustername_developer}"
+}
+
+
+// Science Platform Folder
+resource "google_folder_iam_member" "gcp_science_platform_clustername_admins_iam_permissions" {
+  for_each = toset(var.gcp_science_platform_clustername_admins_iam_permissions)
+  folder  = data.google_active_folder.splatform_sub_folder.name
+  role    = each.value
+  member  = "group:${module.constants.values.gcp_science_platform_clustername_admins}"
+}
+
+resource "google_folder_iam_member" "gcp_science_platform_clustername_developer_iam_permissions" {
+  for_each = toset(var.gcp_science_platform_clustername_developer_iam_permissions)
+  folder  = data.google_active_folder.splatform_sub_folder.name
+  role    = each.value
+  member  = "group:${module.constants.values.gcp_science_platform_clustername_developer}"
+}
+
+// Processing Folder
+resource "google_folder_iam_member" "gcp_processing_clustername_admins_iam_permissions" {
+  for_each = toset(var.gcp_processing_clustername_admins_iam_permissions)
+  folder  = data.google_active_folder.processing_sub_folder.name
+  role    = each.value
+  member  = "group:${module.constants.values.gcp_processing_clustername_admins}"
+}
+resource "google_folder_iam_member" "gcp_processing_clustername_developer_iam_permissions" {
+  for_each = toset(var.gcp_processing_clustername_developer_iam_permissions)
+  folder  = data.google_active_folder.processing_sub_folder.name
+  role    = each.value
+  member  = "group:${module.constants.values.gcp_processing_clustername_developer}"
+}
+
+// Square Folder
+resource "google_folder_iam_member" "gcp_square_clustername_admins_iam_permissions" {
+  for_each = toset(var.gcp_square_clustername_admins_iam_permissions)
+  folder  = data.google_active_folder.square_sub_folder.name
+  role    = each.value
+  member  = "group:${module.constants.values.gcp_square_clutername_admins}"
+}
+
+resource "google_folder_iam_member" "gcp_square_clustername_developer_iam_permissions" {
+  for_each = toset(var.gcp_square_clustername_developer_iam_permissions)
+  folder  = data.google_active_folder.square_sub_folder.name
+  role    = each.value
+  member  = "group:${module.constants.values.gcp_square_clustername_developer}"
+}
