06f478d5361ca693c59d11009796fb195b08b814
Strong <astrong@Burwood.com>
2020-11-24 16:50:07 -0600

    New vars for the iam.tf

--- environment/foundation/1-org-b/variables.tf
+++ environment/foundation/1-org-b/variables.tf
@@ -1,4 +1,110 @@
 # ----------------------------------------
+#   IAM FOLDER VARIABLES
+# ----------------------------------------
+
+// CLUSTER ADMINS
+variable "gcp_qserv_clustername_admins_iam_permissions" {
+  description = "List of permissions granted to the group."
+  type        = list(string)
+  default = [
+    "roles/container.admin",
+    "roles/container.clusterAdmin",
+    "roles/logging.admin",
+    "roles/resourcemanager.projectCreator",
+    "roles/monitoring.admin"
+  ]
+}
+
+variable "gcp_science_platform_clustername_admins_iam_permissions" {
+  description = "List of permissions granted to the group."
+  type        = list(string)
+  default = [
+    "roles/container.admin",
+    "roles/container.clusterAdmin",
+    "roles/logging.admin",
+    "roles/resourcemanager.projectCreator",
+    "roles/monitoring.admin"
+  ]
+}
+
+variable "gcp_processing_clustername_admins_iam_permissions" {
+  description = "List of permissions granted to the group."
+  type        = list(string)
+  default = [
+    "roles/container.admin",
+    "roles/container.clusterAdmin",
+    "roles/logging.admin",
+    "roles/resourcemanager.projectCreator",
+    "roles/monitoring.admin"
+  ]
+}
+
+variable "gcp_square_clustername_admins_iam_permissions" {
+  description = "List of permissions granted to the group."
+  type        = list(string)
+  default = [
+    "roles/container.admin",
+    "roles/container.clusterAdmin",
+    "roles/logging.admin",
+    "roles/resourcemanager.projectCreator",
+    "roles/monitoring.admin"
+  ]
+}
+
+// CLUSTER DEVELOPERS
+variable "gcp_qserv_clustername_developer_iam_permissions" {
+  description = "List of permissions granted to the group."
+  type        = list(string)
+  default = [
+    "roles/container.clusterViewer",
+    "roles/container.viewer",
+    "roles/container.developer",
+    "roles/logging.viewer",
+    "roles/monitoring.editor",
+    "roles/storage.objectViewer",
+  ]
+}
+
+variable "gcp_science_platform_clustername_developer_iam_permissions" {
+  description = "List of permissions granted to the group."
+  type        = list(string)
+  default = [
+    "roles/container.clusterViewer",
+    "roles/container.viewer",
+    "roles/container.developer",
+    "roles/logging.viewer",
+    "roles/monitoring.editor",
+    "roles/storage.objectViewer",
+  ]
+}
+
+variable "gcp_processing_clustername_developer_iam_permissions" {
+  description = "List of permissions granted to the group."
+  type        = list(string)
+  default = [
+    "roles/container.clusterViewer",
+    "roles/container.viewer",
+    "roles/container.developer",
+    "roles/logging.viewer",
+    "roles/monitoring.editor",
+    "roles/storage.objectViewer",
+  ]
+}
+
+variable "gcp_square_clustername_developer_iam_permissions" {
+  description = "List of permissions granted to the group."
+  type        = list(string)
+  default = [
+    "roles/container.clusterViewer",
+    "roles/container.viewer",
+    "roles/container.developer",
+    "roles/logging.viewer",
+    "roles/monitoring.editor",
+    "roles/storage.objectViewer",
+  ]
+}
+
+# ----------------------------------------
 #   SUB FOLDER VARIABLES
 # ----------------------------------------
 
