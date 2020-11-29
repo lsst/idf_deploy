d7e1589837441f8697842574a6393f714c0093a4
Strong <astrong@Burwood.com>
2020-11-29 11:12:08 -0600

    init file

---
+++ environment/deployments/simple-project/outputs.tf
@@ -0,0 +1,24 @@
+output "project_id" {
+    description = "The ID of the created project."
+    value = module.project_factory.project_id
+}
+
+output "project_name" {
+    description = "The name of the created project"
+    value = module.project_factory.project_name
+}
+
+output "project_number" {
+    description = "The number of the created project"
+    value = module.project_factory.project_number
+}
+
+output "enabled_apis" {
+    description = "Enabled APIs in the project"
+    value = module.project_factory.enabled_apis
+}
+
+output "budget_name" {
+    description = "The name of the budget if created"
+    value = module.project_factory.budget_name
+}
