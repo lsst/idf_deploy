026128aabef6ac4b526180ad2aa9bedc6282abf4
Strong <astrong@Burwood.com>
2020-11-29 19:38:41 -0600

    fix network in gke

--- environment/deployments/simple-project/main.tf
+++ environment/deployments/simple-project/main.tf
@@ -14,7 +14,7 @@ module "project_factory" {
 
 module "gke" {
   source            = "../../../modules/gke"
-  network           = module.project_factory.network_name
+  network           = module.project_factory.network
   project_id        = module.project_factory.project_id
   subnetwork        = "subnet-01"
   skip_provisioners = true
