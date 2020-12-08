60fe486c6e098c2bdd18888e190d13dfa537d89d
Strong <astrong@Burwood.com>
2020-12-08 16:34:38 -0600

    update

--- environment/deployments/qserv/env/dev.tfvars
+++ environment/deployments/qserv/env/dev.tfvars
@@ -1,13 +1,13 @@
 # Project
 environment             = "dev"
-application_name        = "science-platform"
-folder_id               = "985686879610"
+application_name        = "qserv"
+folder_id               = "195355585008"
 
 # VPC
-network_name            = "science-platform-dev-vpc"
+network_name            = "qserv-dev-vpc"
 subnets = [
   {
-    "subnet_ip" : "10.128.0.0/23",
+    "subnet_ip" : "10.134.0.0/23",
     "subnet_name" : "subnet-us-central1-01",
     "subnet_region" : "us-central1"
   }
@@ -16,23 +16,14 @@ secondary_ranges = {
   "subnet-us-central1-01" : [
     {
       range_name    = "kubernetes-pods"
-      ip_cidr_range = "10.129.0.0/16"
+      ip_cidr_range = "10.135.0.0/16"
     },
     {
       range_name    = "kubernetes-services"
-      ip_cidr_range = "10.128.16.0/20"
+      ip_cidr_range = "10.134.16.0/20"
     },
   ]
 }
-
-# GKE
-master_ipv4_cidr_block = "172.16.0.0/28"
-node_pool_1_name = "core-pool"
-node_pool_1_machine_type = "e2-standard-4" # 4 vCPU 16GB RAM
-node_pool_1_min_count = 1
-node_pool_1_max_count = 15
-node_pool_1_disk_size_gb = 100
-node_pool_1_initial_node_count = 5
 
 # Filestore
 fileshare_capacity = 2000
--- environment/deployments/qserv/variables.tf
+++ environment/deployments/qserv/variables.tf
@@ -15,16 +15,6 @@ variable "billing_account" {
   default     = "01122E-72D62B-0B0581"
 }
 
-# variable "project_prefix" {
-#   description = "The name of the GCP project"
-#   type        = string
-# }
-
-# variable "cost_centre" {
-#   description = "The cost centre that links to the application"
-#   type        = string
-# }
-
 variable "application_name" {
   description = "The name of application where GCP resources relate"
   type        = string
@@ -48,11 +38,6 @@ variable "environment" {
   description = "The environment the single project belongs to"
   type        = string
 }
-
-# variable "owner" {
-#   description = "The owner of the project."
-#   type        = string
-# }
 
 variable "skip_gcloud_download" {
   description = "Whether to skip downloading gcloud (assumes gcloud is already available outside the module)"
@@ -137,88 +122,6 @@ variable "secondary_ranges" {
   }
 }
 
-# GKE
-
-variable "master_ipv4_cidr_block" {
-  default = "172.16.0.0/28"
-}
-
-variable "zones" {
-  description = "The zones to host the cluster in (optional if regional cluster / required if zonal)"
-  type        = list(string)
-  default     = ["us-central1-a"]
-}
-
-variable "maintenance_start_time" {
-  description = "Time window specified for daily maintenance operations in RFC3339 format"
-  type        = string
-  default     = "05:00"
-}
-
-variable "node_pool_1_name" {
-  default = "core-pool"
-}
-
-variable "node_pool_1_image_type" {
-  default = "cos_containerd"
-}
-
-variable "node_pool_1_enable_secure_boot" {
-  description = "Secure Boot helps ensure that the system only runs authentic software by verifying the digital signature of all boot components, and halting the boot process if signature verification fails."
-  default     = true
-}
-
-variable "node_pool_1_machine_type" {
-  default = "g1-small"
-}
-
-variable "node_pool_1_min_count" {
-  default = 1
-}
-
-variable "node_pool_1_max_count" {
-  default = 15
-}
-
-variable "node_pool_1_local_ssd_count" {
-  default = 0
-}
-
-variable "node_pool_1_disk_size_gb" {
-  default = 100
-}
-
-variable "node_pool_1_initial_node_count" {
-  default = 1
-}
-
-# variable "node_pools" {
-#   type        = list(map(string))
-#   description = "List of maps containing node pools"
-
-#   default = [{}]
-# }
-
-variable "cluster_resource_labels" {
-  type        = map(string)
-  description = "The GCE resource labels (a map of key/value pairs) to be applied to the cluster"
-  default = {
-    owner       = "owner_here"
-    environment = "environment"
-  }
-}
-
-variable "node_pools_labels" {
-  type        = map(map(string))
-  description = "Map of maps containing node labels by node-pool name"
-  default = {
-    all = {
-      owner       = "owner_here"
-      environment = "environment_here"
-    }
-  }
-}
-
 # FileStore
 
 variable "name" {
@@ -274,12 +177,4 @@ variable "router_name" {
   type        = string
   description = "Name of the router"
   default     = "cloud-router"
-}
-
-# STATIC IP RESERVATION
-
-variable "static_ip_name" {
-  description = "Name to give to the static ip"
-  type = string
-  default = "load-balancer"
 }
