193c083ffc6743a15bafec2d446c9d0a5c3c4571
Strong <astrong@Burwood.com>
2020-12-13 15:46:14 -0600

    removed unused mod

--- modules/ip_reservation/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  value       = google_compute_address.static_ip.id
-  description = "The id of the resource"
-}
-
-output "self_link" {
-  value       = google_compute_address.static_ip.self_link
-  description = "The URI of the created resource"
-}
