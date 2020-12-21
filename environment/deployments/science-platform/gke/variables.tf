5552acbf583ef5058c6cb786b116f92e654f416e
Strong <astrong@Burwood.com>
2020-12-21 16:28:52 -0600

    removed vars for filestore

--- environment/deployments/science-platform/gke/variables.tf
+++ environment/deployments/science-platform/gke/variables.tf
@@ -10,16 +10,6 @@ variable "environment" {
   type        = string
 }
 
-# # TF State in GCS for data blocks
-
-# variable bucket {
-#   description = "GCS bucket name that stores state"
-# }
-
-# variable prefix {
-#   description = "GCS folder name"
-# }
-
 # VPC
 
 variable "network_name" {
