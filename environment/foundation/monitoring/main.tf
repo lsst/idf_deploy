c8dcc931a7fdc6def8b09238accd1c902a708a93
dspeck1 <dspeck@burwood.com>
2021-01-18 16:33:18 -0600

    fixed missing project var

--- environment/foundation/monitoring/main.tf
+++ environment/foundation/monitoring/main.tf
@@ -1,18 +1,20 @@
 module "cloud_storage_dashboard" {
-    source                  = "./modules/gcs"
-    project                 = var.project
-    gcs_dashboard_enable    = var.gcs_dashboard_enable
-    gcs_dashboard_name      = var.gcs_dashboard_name  
+    source                    = "./modules/gcs"
+    project                   = var.project
+    gcs_dashboard_enable      = var.gcs_dashboard_enable
+    gcs_dashboard_name        = var.gcs_dashboard_name  
 }
 
 module "cloudsql_dashboard" {
     source                    = "./modules/cloudsql"
+    project                   = var.project
     cloudsql_dashboard_enable = var.cloudsql_dashboard_enable
     cloudsql_dashboard_name   = var.cloudsql_dashboard_name   
 }
 
 module "postgres_dashboard" {
     source                    = "./modules/postgres"
+    project                   = var.project
     postgres_dashboard_enable = var.postgres_dashboard_enable
     postgres_dashboard_name   = var.postgres_dashboard_name   
 }
