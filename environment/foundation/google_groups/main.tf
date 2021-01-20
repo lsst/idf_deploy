10e811df42090ae0a621c20151b90874e04ea568
Strong <astrong@Burwood.com>
2021-01-20 08:14:18 -0600

    destroy group

--- environment/foundation/google_groups/main.tf
+++ environment/foundation/google_groups/main.tf
@@ -1,11 +1,11 @@
 module "group" {
   source  = "../../../modules/google_groups"
   
-#   id           = var.id
-#   display_name = var.display_name
-#   description  = var.description
-#   domain       = var.domain
-#   owners       = var.owners
-#   managers     = var.managers
-#   members      = var.members
+  id           = var.id
+  display_name = var.display_name
+  description  = var.description
+  domain       = var.domain
+  owners       = var.owners
+  managers     = var.managers
+  members      = var.members
 }
