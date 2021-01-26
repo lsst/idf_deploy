0087278672eb1fccdd4bc9eabda58a14adef297f
Strong <astrong@Burwood.com>
2021-01-26 13:24:52 -0600

    module only allows for creation between local peer and remote. Updated main.tf and tfvars to reflect

--- environment/deployments/butler-repo/vpc_peering/main.tf
+++ environment/deployments/butler-repo/vpc_peering/main.tf
@@ -25,16 +25,16 @@ data "google_compute_network" "remote_peer_project" {
   name    = var.remote_network_name
 }
 
-// Filter project based on the project labels of the remote project
-data "google_projects" "remote_peer_project2" {
-  filter = "labels.application_name=${var.remote_application_name2} labels.environment=${var.remote_environment2}"
-}
+# // Filter project based on the project labels of the remote project
+# data "google_projects" "remote_peer_project2" {
+#   filter = "labels.application_name=${var.remote_application_name2} labels.environment=${var.remote_environment2}"
+# }
 
-// Get the self link of the VPC in local project
-data "google_compute_network" "remote_peer_project2" {
-  project = data.google_projects.remote_peer_project2.projects[0].project_id
-  name    = var.remote_network_name2
-}
+# // Get the self link of the VPC in local project
+# data "google_compute_network" "remote_peer_project2" {
+#   project = data.google_projects.remote_peer_project2.projects[0].project_id
+#   name    = var.remote_network_name2
+# }
 
 # ----------------------------------------
 #   VPC PEERING
@@ -58,22 +58,24 @@ module "peering-2" {
 
   module_depends_on = [module.peering-1.complete]
 }
-module "peering-3" {
-  source = "../../../../modules/vpc_peering"
 
-  local_network = data.google_compute_network.local_network.self_link
-  peer_network  = data.google_compute_network.remote_peer_project2.self_link
 
-  export_local_custom_routes = true
-}
+# module "peering-3" {
+#   source = "../../../../modules/vpc_peering"
 
-module "peering-4" {
-  source = "../../../../modules/vpc_peering"
+#   local_network = data.google_compute_network.local_network.self_link
+#   peer_network  = data.google_compute_network.remote_peer_project2.self_link
 
-  local_network = data.google_compute_network.remote_peer_project2.self_link
-  peer_network  = data.google_compute_network.local_network.self_link
+#   export_local_custom_routes = true
+# }
 
-  export_local_custom_routes = true
+# module "peering-4" {
+#   source = "../../../../modules/vpc_peering"
 
-  module_depends_on = [module.peering-3.complete]
-}
+#   local_network = data.google_compute_network.remote_peer_project2.self_link
+#   peer_network  = data.google_compute_network.local_network.self_link
+
+#   export_local_custom_routes = true
+
+#   module_depends_on = [module.peering-3.complete]
+# }
