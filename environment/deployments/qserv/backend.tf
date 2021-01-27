cfc2c5230f929692b696d6b17d4d2117efc525e1
Strong <astrong@Burwood.com>
2021-01-27 09:31:26 -0600

    trying provider 3.52

--- environment/deployments/qserv/backend.tf
+++ environment/deployments/qserv/backend.tf
@@ -5,7 +5,7 @@
 terraform {
   backend "gcs" {}
   required_providers {
-    google      = "~> 3.54.0"
-    google-beta = "~> 3.54.0"
+    google      = "~> 3.52.0"
+    google-beta = "~> 3.52.0"
   }
 }
