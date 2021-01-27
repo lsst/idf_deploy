90aab80d66b437b2a8ec392dc07569be5525cab0
Strong <astrong@Burwood.com>
2021-01-27 09:27:21 -0600

    pinned version to 3.54 to reproduce issue

--- environment/deployments/qserv/backend.tf
+++ environment/deployments/qserv/backend.tf
@@ -5,7 +5,7 @@
 terraform {
   backend "gcs" {}
   required_providers {
-    google      = "~> 3.51.0"
-    google-beta = "~> 3.51.0"
+    google      = "~> 3.54.0"
+    google-beta = "~> 3.54.0"
   }
 }
