b6cf669bd24203d2191d2d95b4f2f86552a0265f
podolsky <spodolsky@mail.ru>
2021-03-17 12:25:39 -0400

    Added development cluster

--- environment/deployments/panda/gke/main.tf
+++ environment/deployments/panda/gke/main.tf
@@ -110,4 +110,36 @@ module "gke_2" {
   }
 }
 
+module "gke_dev" {
+  source = "../../../../modules/gke"
 
+  # Cluster
+  name                   = "developmentcluster"
+  project_id             = local.project_id
+  network                = var.network_name
+  subnetwork             = local.subnetwork_3
+  master_ipv4_cidr_block = var.master_ipv4_cidr_block_3
+  release_channel        = var.release_channel
+  node_pools             = var.node_pools
+  network_policy         = var.network_policy
+  gce_pd_csi_driver      = var.gce_pd_csi_driver
+  cluster_telemetry_type = var.cluster_telemetry_type
+  zones                  = var.zones
+
+  # Labels
+  cluster_resource_labels = {
+    environment      = var.environment
+    project          = local.project_id
+    application_name = var.application_name
+    subnetwork       = local.subnetwork_3
+  }
+
+  # Node Pools
+  node_pools_labels = {
+    all = {
+      environment      = var.environment
+      project          = local.project_id
+      application_name = var.application_name
+    }
+  }
+}
