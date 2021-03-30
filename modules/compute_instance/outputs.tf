fc42afa956213fb82569097f18935a5caa1a7e7a
Strong <astrong@Burwood.com>
2021-03-30 10:53:17 -0500

    new outputs

--- modules/compute_instance/outputs.tf
+++ modules/compute_instance/outputs.tf
@@ -12,3 +12,8 @@ output "available_zones" {
   description = "List of available zones in a region"
   value       = data.google_compute_zones.available.names
 }
+
+output "instance_zone" {
+  description = "The zone the instance deployed into"
+  value = google_compute_instance.default.*.zone
+}
