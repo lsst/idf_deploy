c28fa937285ee4711f2b5d6cbd8fdcc7c19040f5
Hsin-Fang Chiang <hchiang@lsst.org>
2021-05-13 15:59:55 -0700

    Add a service account for the qserv-dev bucket

--- environment/deployments/qserv/bucket/main.tf
+++ environment/deployments/qserv/bucket/main.tf
@@ -9,3 +9,21 @@ module "storage_bucket" {
   force_destroy = var.force_destroy
   labels        = var.labels
 }
+
+module "bucket_service_account" {
+  source = "../../../../modules/service_accounts/"
+
+  project_id   = var.project_id
+  prefix       = var.prefix_name
+  names        = ["argo-artifact"]
+  display_name = "GCS Service account for qserv-dev"
+  description  = "GCS access service account managed by Terraform"
+  project_roles = []
+}
+
+// RW storage access to the argo artifact bucket
+resource "google_storage_bucket_iam_member" "qserv_dev_rw_argo_artifact" {
+  bucket = "qserv-us-central1-argo-artifact"
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${module.bucket_service_account.email}"
+}
