572264794f1bbcb028e2c99289a02cb0b75ddf23
Spencer Nelson <swnelson@uw.edu>
2021-10-21 11:03:19 -0700

    Enable admission webhooks in alert-prod gke
    Per https://github.com/kubernetes/kubernetes/issues/79739, access to port 8443
    from cluster master nodes needs to be explicitly enabled in GKE in order to use
    admission webhooks from the nginx ingress controller.

--- environment/deployments/alert-prod/gke/main.tf
+++ environment/deployments/alert-prod/gke/main.tf
@@ -64,3 +64,32 @@ module "gke" {
     }
   }
 }
+
+module "firewall" {
+  # Permit tcp/8443 from the cluster master nodes so that nginx ingress
+  # controller can use admission webhooks.
+  #
+  # https://github.com/kubernetes/kubernetes/issues/79739
+  source = "../../../../modules/firewall"
+
+  network    = var.network_name
+  project_id = local.project_id
+
+  custom_rules = {
+    gke_admission_webooks = {
+      description          = "Deployed with Terraform"
+      direction            = "INGRESS"
+      action               = "allow"
+      ranges               = [local.master_ipv4_cidr_block]
+      targets              = [module.gke.name]
+      use_service_accounts = false
+      rules = [
+        {
+          protocol = "tcp"
+          ports    = ["8443"]
+        },
+      ]
+      extra_attributes = {}
+    }
+  }
+}
