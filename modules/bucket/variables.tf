c4b349a4fe0c2ccf59ff27d54ef5659a37dcae17
Spencer Nelson <swnelson@uw.edu>
2021-12-15 11:59:46 -0800

    Add support for lifecycle rules to buckets

--- modules/bucket/variables.tf
+++ modules/bucket/variables.tf
@@ -116,3 +116,52 @@ variable "viewers" {
   type        = list(string)
   default     = []
 }
+
+variable "lifecycle_rules" {
+  description = "List of lifecycle rules to configure. Format is the same as described in provider documentation https://www.terraform.io/docs/providers/google/r/storage_bucket.html#lifecycle_rule except condition.matches_storage_class should be a comma delimited string."
+  default = {}
+  type = set(object({
+    # Object with keys:
+    # - type - The type of the action of this Lifecycle Rule. Supported values:
+    #       Delete and SetStorageClass.
+    #
+    # - storage_class - (Required if action type is SetStorageClass) The target
+    #       Storage Class of objects affected by this Lifecycle Rule.
+    action = map(string)
+
+    # Object with keys:
+    # - age - (Optional) Minimum age of an object in days to satisfy this condition.
+    #
+    # - created_before - (Optional) Creation date of an object in RFC 3339
+    #       (e.g. 2017-06-13) to satisfy this condition.
+    #
+    # - with_state - (Optional) Match to live and/or archived objects.
+    #       Supported values include: "LIVE", "ARCHIVED", "ANY".
+    #
+    # - matches_storage_class - (Optional) Comma delimited string for storage
+    #      class of objects to satisfy this condition. Supported values
+    #      include: MULTI_REGIONAL, REGIONAL, NEARLINE, COLDLINE, STANDARD,
+    #      DURABLE_REDUCED_AVAILABILITY.
+    #
+    # - num_newer_versions - (Optional) Relevant only for versioned objects.
+    #       The number of newer versions of an object to satisfy this condition.
+    #
+    # - custom_time_before - (Optional) A date in the RFC 3339 format
+    #       YYYY-MM-DD. This condition is satisfied when the customTime
+    #       metadata for the object is set to an earlier date than the date
+    #       used in this lifecycle condition.
+    #
+    # - days_since_custom_time - (Optional) The number of days from the
+    #       Custom-Time metadata attribute after which this condition becomes
+    #       true.
+    #
+    # - days_since_noncurrent_time - (Optional) Relevant only for versioned
+    #       objects. Number of days elapsed since the noncurrent timestamp of
+    #       an object.
+    #
+    # - noncurrent_time_before - (Optional) Relevant only for versioned
+    #       objects. The date in RFC 3339 (e.g. 2017-06-13) when the object
+    #       became nonconcurrent.
+    condition = map(string)
+  }))
+}
