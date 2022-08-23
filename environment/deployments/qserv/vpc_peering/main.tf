db01de9834286e0a2d87033e1ebbe1c3003af0d8
Aaron Strong <astrong@Burwood.com>
2022-08-23 11:24:18 -0500

    new vars for a new vpc-peer

--- environment/deployments/qserv/vpc_peering/main.tf
+++ environment/deployments/qserv/vpc_peering/main.tf
@@ -25,6 +25,17 @@ data "google_compute_network" "remote_peer_project" {
   name    = var.remote_network_name
 }
 
+// Filter project based on the project labels of the remote project
+data "google_projects" "remote2_peer_project" {
+  filter = "labels.application_name=${var.remote2_application_name} labels.environment=${var.remote2_environment}"
+}
+
+// Get the self link of the VPC in local project
+data "google_compute_network" "remote2_peer_project" {
+  project = data.google_projects.remote2_peer_project.projects[0].project_id
+  name    = var.remote2_network_name
+}
+
 # ----------------------------------------
 #   VPC PEERING
 # ----------------------------------------
@@ -46,4 +57,27 @@ module "peering-2" {
   export_local_custom_routes = true
 
   module_depends_on = [module.peering-1.complete]
+}
+
+
+module "peering-3" {
+  # qserv-int <-> rsp-dev
+  source = "../../../../modules/vpc_peering"
+
+  local_network = data.google_compute_network.local_network.self_link
+  peer_network  = data.google_compute_network.remote2_peer_project.self_link
+
+  export_local_custom_routes = true
+}
+
+module "peering-4" {
+  # rsp-dev <-> qserv-int
+  source = "../../../../modules/vpc_peering"
+
+  local_network = data.google_compute_network.remote2_peer_project.self_link
+  peer_network  = data.google_compute_network.local_network.self_link
+
+  export_local_custom_routes = true
+
+  module_depends_on = [module.peering-3.complete]
 }
