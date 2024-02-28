04dcf4ebfcf0de49e2539ec7a4e5d88fc1c71bc4
adam <athornton@lsst.org>
2024-02-28 14:20:38 -0700

    Add additional roles for bucket backup service

--- environment/deployments/roundtable/main.tf
+++ environment/deployments/roundtable/main.tf
@@ -141,6 +141,36 @@ resource "google_storage_bucket_iam_member" "vault_server_storage_backup_sa" {
   member  = "serviceAccount:vault-server@${module.project_factory.project_id}.iam.gserviceaccount.com"
 }
 
+// Hidden SA for data transfer job
+
+data "google_storage_transfer_project_service_account" "vault_backup_transfer_sa" {
+  project = var.project"
+}
+
+resource "google_storage_bucket_iam_member" "vault_server_storage_transfer_source_sa" {
+  bucket  = module.storage_bucket.name
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:${data.google_storage_transfer_project_service_account.vault_backup_transfer_sa}.iam.gserviceaccount.com"
+}
+
+resource "google_storage_bucket_iam_member" "vault_server_storage_transfer_source_sa_r" {
+  bucket  = module.storage_bucket.name
+  role    = "roles/storage.legacyBucketReader"
+  member  = "serviceAccount:${data.google_storage_transfer_project_service_account.vault_backup_transfer_sa}.iam.gserviceaccount.com"
+}
+
+resource "google_storage_bucket_iam_member" "vault_server_storage_transfer_sink_sa" {
+  bucket  = module.storage_bucket_b.name
+  role    = "roles/storage.legacyBucketWriter"
+  member  = "serviceAccount:${data.google_storage_transfer_project_service_account.vault_backup_transfer_sa}.iam.gserviceaccount.com"
+}
+
+resource "google_storage_bucket_iam_member" "vault_server_storage_transfer_sink_sa_r" {
+  bucket  = module.storage_bucket_b.name
+  role    = "roles/storage.legacyBucketReader"
+  member  = "serviceAccount:${data.google_storage_transfer_project_service_account.vault_backup_transfer_sa}.iam.gserviceaccount.com"
+}
+
 // Resources for Vault Server storage backups
 
 resource "google_storage_transfer_job" "vault_server_storage_backup" {
