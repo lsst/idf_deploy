21f6e83baad7d7774ca1bc5c0399e51b0705727a
adam <athornton@lsst.org>
2025-03-26 09:47:07 -0700

    Temporarily disable outputs

--- modules/netapp_volumes/outputs.tf
+++ modules/netapp_volumes/outputs.tf
@@ -1,24 +1,13 @@
-output "pool_name" {
-  description = "The name of the volume pool"
-  value       = netapp_storage_pool.instance.name
-}
-
-output "volume_name" {
-  description = "The name of the volume"
-  value       = netapp_storage_pool.volume.name
-}
-
-output "capacity_gib" {
-  description = "The volume/pool capacity"
-  value       = netapp_storage_pool.capacity_gib
-}
-
-output "id" {
-  description = "An identifier for the resource"
-  value       = netapp_storage_pool.instance.id
-}
-
-output "mount_point" {
-  description = "How to mount this volume"
-  value       = netapp_volume.instance.mount_options.export_full
-}
+# locals {
+#   vol_names = flatten([
+#     for vdef in var.definitions : [
+#       vdef.name
+#     ]
+#   ])
+# }
+    
+# output "mount_points" {
+#   description = "How to mount this volume"
+#   for_each    = local.vol_names
+#   value       = netapp_volume.mount_options.export_full
+# }
