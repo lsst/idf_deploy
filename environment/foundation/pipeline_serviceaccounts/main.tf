2addfcec72a6712fef80a203a9b95f6479d0678a
Dan Fuchs <danfuchs@slac.stanford.edu>
2025-10-22 16:42:46 -0500

    Add GKE Backup permissions to GitHub pipelines accounts
    GitHub pipelines should be using specific service accounts for each
    environment when applying Terraform config. These accounts now need
    permissions on GKE Backup resources.
    
    Some pipelines are still using the `terraform-sa` account, which should
    only be used when bootstrapping environments, which is why the apply of
    these GKE backup resources worked in some environments.
    
    Note that there is no pipeline service account for `idfprod`, so that's
    why there is no change in this PR for `idfprod`.

--- environment/foundation/pipeline_serviceaccounts/main.tf
+++ environment/foundation/pipeline_serviceaccounts/main.tf
@@ -16,6 +16,7 @@ module "rsp_demo_gke_pipeline_accounts" {
     "science-platform-demo-9e05=>roles/container.admin",
     "science-platform-demo-9e05=>roles/container.clusterAdmin",
     "science-platform-demo-9e05=>roles/iam.serviceAccountUser",
+    "science-platform-demo-9e05=>roles/gkebackup.backupAdmin",
   ]
 }
 // Storage access to read tfstate
@@ -76,6 +77,7 @@ module "rsp_dev_gke_pipeline_accounts" {
     "science-platform-dev-7696=>roles/container.admin",
     "science-platform-dev-7696=>roles/container.clusterAdmin",
     "science-platform-dev-7696=>roles/iam.serviceAccountUser",
+    "science-platform-dev-7696=>roles/gkebackup.backupAdmin",
   ]
 }
 // Storage access to read tfstate
@@ -135,6 +137,7 @@ module "rsp_int_gke_pipeline_accounts" {
     "science-platform-int-dc5d=>roles/container.admin",
     "science-platform-int-dc5d=>roles/container.clusterAdmin",
     "science-platform-int-dc5d=>roles/iam.serviceAccountUser",
+    "science-platform-int-dc5d=>roles/gkebackup.backupAdmin",
   ]
 }
 // Storage access to read tfstate
