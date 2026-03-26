05b9a62248014769f9354f7325553aa338d32def
dspeck1 <dspeck@burwood.com>
2026-03-26 13:46:53 -0500

    Move locals to main.

--- environment/foundation/1-org/main.tf
+++ environment/foundation/1-org/main.tf
@@ -4,3 +4,8 @@
 module "constants" {
   source = "../constants"
 }
+
+locals {
+  parent_resource_id   = var.parent_folder != "" ? var.parent_folder : module.constants.values.org_id
+  parent_resource_type = var.parent_folder != "" ? "folder" : "organization"
+}
