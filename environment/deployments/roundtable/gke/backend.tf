e56c2c07f110c2dd671edff91fe8843a11ead6e2
Dan Fuchs <danfuchs@slac.stanford.edu>
2026-04-07 14:29:53 -0500

    Bump roundtable GKE Google provider versions to ~> 7.0

--- environment/deployments/roundtable/gke/backend.tf
+++ environment/deployments/roundtable/gke/backend.tf
@@ -7,11 +7,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.26.0"
+      version = "~> 7.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 6.26.0"
+      version = "~> 7.0"
     }
   }
 }
