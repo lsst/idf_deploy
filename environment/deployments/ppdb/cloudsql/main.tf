a5568b1f6c7688630be810249404a9b057b1e6c8
dspeck1 <dspeck@burwood.com>
2026-05-20 15:21:15 -0500

    Set prefix to be populated by application and environment variables.

--- environment/deployments/ppdb/cloudsql/main.tf
+++ environment/deployments/ppdb/cloudsql/main.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "ppdb_project" {
   backend = "gcs"
 
   config = {
-    prefix = "ppdb/dev"
+    prefix = "${var.application_name}/${var.environment}"
     bucket = var.state_bucket
   }
 }
